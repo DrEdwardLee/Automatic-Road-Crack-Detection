3be1517f889961e56a429c023e737ce773c5b91e
David Fernandes <davidluisdias@gmail.com>
2017-09-01 19:05:15 +0100

    Added plots with new features and changes in the block detect method

--- block_detectV2.m
+++ block_detectV2.m
@@ -1,4 +1,4 @@
-function [ res, mean_std_crack_block, mean_mean_crack_block, mean_std_no_crack_block, mean_mean_no_crack_block ] = block_detectV2( image, num_of_blocks, train_mean, sigma_m )
+function [ res, mean_std_crack_block, mean_mean_crack_block, mean_std_no_crack_block, mean_mean_no_crack_block ] = block_detectV2( image, num_of_blocks, moment_order, train_mean, sigma_m, train_std, sigma_s )
 row_size = size(image, 1);
 column_size = size(image, 2);
 
@@ -7,6 +7,8 @@ column_block_size = floor(column_size/num_of_blocks);
 
 mean_block = zeros(num_of_blocks*num_of_blocks,1);
 std_block = zeros(num_of_blocks*num_of_blocks,1);
+min_block = zeros(num_of_blocks*num_of_blocks,1);
+moment_block = zeros(num_of_blocks*num_of_blocks,1);
 res = zeros(row_size, column_size);
 
 k = 1;
@@ -24,6 +26,8 @@ for m = 1:num_of_blocks
      
         mean_block(k) = mean2(image(l:p,q:o));        
         std_block(k) = std2(image(l:p,q:o));
+        min_block(k) = max(max(image(l:p,q:o)));
+        moment_block(k) = moment(moment(double(image(l:p,q:o)), moment_order), moment_order);
         
         k = k + 1;
     end
@@ -63,6 +67,36 @@ mean_std_no_crack_block = mean(std_block(find(~block_label)));
 mean_mean_no_crack_block = mean(mean_block(find(~block_label)));
 
 
+figure;
+subplot(1,3,1)
+scatter(std_block(find(block_label)), mean_block(find(block_label)));
+ylabel(' Block Mean Value ') % x-axis label
+xlabel('Block Std Value') % y-axis label
+hold on;
+scatter(train_std*sigma_s, train_mean*sigma_m);
+scatter(std_block(find(~block_label)), mean_block(find(~block_label)));
+legend('Blocks labeled with Crack','Blocks labeled without Crack')
+hold off;
+
+subplot(1,3,2)
+scatter(min_block(find(block_label)), mean_block(find(block_label)));
+ylabel(' Block Mean Value ') % x-axis label
+xlabel('Block Min Value') % y-axis label
+hold on;
+scatter(train_std*sigma_s, train_mean*sigma_m);
+scatter(min_block(find(~block_label)), mean_block(find(~block_label)));
+legend('Blocks labeled with Crack','Blocks labeled without Crack')
+hold off;
+
+subplot(1,3,3)
+scatter(moment_block(find(block_label)), mean_block(find(block_label)));
+ylabel(' Block Mean Value ') % x-axis label
+xlabel('Block Moment Value') % y-axis label
+hold on;
+scatter(train_std*sigma_s, train_mean*sigma_m);
+scatter(moment_block(find(~block_label)), mean_block(find(~block_label)));
+legend('Blocks labeled with Crack','Blocks labeled without Crack')
+hold off;
 
 end
 
